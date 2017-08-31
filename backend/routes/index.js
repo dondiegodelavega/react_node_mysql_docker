dbcce7971138246ebaa9ce5f74bc6242d3dbc639
INTERNAL\hamidreza.momeni <hamidreza.momeni@pixray.com>
2017-08-31 15:14:44 +0200

    - docker nodemon added - test connection to database

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mysql      = require('mysql');
 
+var connection = mysql.createConnection({
+  host     : 'database',
+  user     : 'root',
+  password : 'root',
+  database : 'React'
+});
+ 
+connection.connect();
+ 
+connection.query('SHOW TABLES', function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
+ 
+connection.end();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
