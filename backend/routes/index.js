3107cd236709941efd49873cf796797bbd3a55a6
INTERNAL\hamidreza.momeni <hamidreza.momeni@pixray.com>
2017-08-31 14:50:55 +0200

    - nodemon added to nodejs docker - mssql conncetion with test done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+var sql = require('mssql');
+
+var dbConfig = {
+  server: "london",
+  database: "PixraySeekerDB_HM",
+  user: "PixraySeeker",
+  password: "PixraySeeker",
+  port: "1433"
+};
+
+
+function getData(){
+  var conn = new sql.ConnectionPool(dbConfig);
+  var req = new sql.Request(conn);
+
+  conn.connect(function(err){
+    if(err){
+      console.log(err);
+      return;
+    }
+
+    req.query("SELECT * FROM UserRole", function(err, recordset){
+      if(err){
+        console.log(err);
+      }else{
+        console.log(recordset);
+      }
+      conn.close();
+    });
+  });
+}
+
+getData();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
